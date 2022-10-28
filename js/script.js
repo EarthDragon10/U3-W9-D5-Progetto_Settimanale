14edd41488dc4ca1240c19bbcf6fb5e41e2c7f51
EarthDragon10 <lagudadev@gmail.com>
2022-10-28 11:18:45 +0200

    add logic ts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 "use strict";
+class CellPhone {
+    constructor(carica) {
+        this.carica = 0; // credito disponibile per le chiamate
+        this.numeroChiamate = 0; // sono il numero di chiamate effettuate
+        this.carica = carica;
+        // this.numeroChiamate = numeroChiamate;
+    }
+}
+class Motorola extends CellPhone {
+    constructor(carica) {
+        super(carica);
+        this.costoSingolaChiamata = 0.2;
+    }
+    ricarica(ricaricaEffettuata) {
+        this.carica += ricaricaEffettuata;
+    }
+    chiamata(minutiDurata) {
+        let costoTotaleChiamata = minutiDurata * this.costoSingolaChiamata;
+        this.numeroChiamate++;
+        this.carica -= costoTotaleChiamata;
+    } // effettua una chiamata di tot minuti, passato come parametro
+    number404() {
+        return this.carica;
+    } // restituisce il valore del credito disponibile
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+    } // deve restituire il valore della variabile numeroChiamate
+    azzeraChiamate() {
+        this.numeroChiamate = 0;
+    } // azzera il valore della variabile "numeroChiamate"
+}
--- ts/script.ts
+++ ts/script.ts
@@ -4,13 +4,50 @@ interface ISmartphone {
 }
 
 abstract class CellPhone implements ISmartphone {
-	carica: number; // credito disponibile per le chiamate
-	numeroChiamate: number; // sono il numero di chiamate effettuate
+	carica: number = 0; // credito disponibile per le chiamate
+	numeroChiamate: number = 0; // sono il numero di chiamate effettuate
 
-	constructor(carica: number, numeroChiamate: number) {
+	constructor(carica: number) {
 		this.carica = carica;
-		this.numeroChiamate = numeroChiamate;
+		// this.numeroChiamate = numeroChiamate;
 	}
 
-	abstract ricarica(ricaricaEffettuata: number): void;
+	abstract ricarica(ricaricaEffettuata: number): void; // ricarica il credito del telefono
+	abstract chiamata(minutiDurata: number): void; // effettua una chiamata di tot minuti, passato come parametro
+	abstract number404(): number; // restituisce il valore del credito disponibile
+	abstract getNumeroChiamate(): number; // deve restituire il valore della variabile numeroChiamate
+	abstract azzeraChiamate(): void; // azzera il valore della variabile "numeroChiamate"
+}
+
+class Motorola extends CellPhone {
+	costoSingolaChiamata: number = 0.2;
+
+	constructor(carica: number) {
+		super(carica);
+	}
+
+	ricarica(ricaricaEffettuata: number): void {
+		this.carica += ricaricaEffettuata;
+	}
+
+	chiamata(minutiDurata: number): void {
+		let costoTotaleChiamata: number =
+			minutiDurata * this.costoSingolaChiamata;
+
+		this.numeroChiamate++;
+
+		this.carica -= costoTotaleChiamata;
+	} // effettua una chiamata di tot minuti, passato come parametro
+
+	number404(): number {
+		return this.carica;
+	} // restituisce il valore del credito disponibile
+
+	getNumeroChiamate(): number {
+		return this.numeroChiamate;
+	} // deve restituire il valore della variabile numeroChiamate
+
+	azzeraChiamate(): void {
+		this.numeroChiamate = 0;
+	} // azzera il valore della variabile "numeroChiamate"
 }
